2056293cb3fb287786c5d6760dcf2c42872a5854
Depanri Purba <depanri.purba@gmail.com>
2024-06-10 23:02:41 +0700

    add logo

--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -3,10 +3,10 @@
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
+        <div class="sidebar-brand-icon">
+            <img width="50" src="<?=base_url('assets/img/kerbau.png')?>" alt="">
         </div>
-        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+        <div class="sidebar-brand-text mx-3">Pakar Kerbau</div>
     </a>
 
     <!-- Divider -->
