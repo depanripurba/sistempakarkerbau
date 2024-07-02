0283ca3013d78aa8834dd47d373ff647d53d82dc
Depanri Purba <depanri.purba@gmail.com>
2024-07-02 22:08:50 +0700

    update

--- application/views/admin/datagejala.php
+++ application/views/admin/datagejala.php
@@ -19,7 +19,8 @@
                         <tr>
                         <td><?=$item['kode_gejala']?></td>
                         <td><?=$item['nama_gejala']?></td>
-                        <td style="width: 200px;"><button data-url="<?= base_url('deletegejala/' . $item['kode_gejala']) ?>" id="hapusdata" class="btn btn-danger">Delete</button> <a href="<?=base_url('editdatagejala/'.$item['kode_gejala'])?>" class="btn btn-primary">Edit</a></td>
+                        <td style="width: 200px;">
+                            <button data-url="<?= base_url('deletegejala/' . $item['kode_gejala']) ?>" id="hapusdata" class="btn btn-danger">Delete</button> <a href="<?=base_url('editdatagejala/'.$item['kode_gejala'])?>" class="btn btn-primary">Edit</a></td>
                         </tr>
                         <?php endforeach?>
                     </tbody>
